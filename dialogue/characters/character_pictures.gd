61fee8476bc1a3637900f35065f8c6faf17bc876
amidos2006 <amidos2002@hotmail.com>
2025-08-07 21:31:57 +0300

    cleaning

--- dialogue/characters/character_pictures.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-class_name CharacterPictures
-
-@export var pictures:Array[Texture]
--- dialogue/prompt_flicker.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _ready():
-	while true:
-		show()
-		await get_tree().create_timer(1).timeout
-		hide()
-		await get_tree().create_timer(1).timeout
