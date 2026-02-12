0a046d46ab2868ce3ee6a8ebfc7eecc059d6d9ea
amidos2006 <amidos2002@hotmail.com>
2026-02-12 12:09:12 +0100

    adding vhs rewind effect

--- end/end.gd
+++ end/end.gd
@@ -1,10 +1,16 @@
-extends Node
+extends CanvasLayer
 
 
 @export var end_actions:JSON
 
 
 func _ready():
-	$Camera2D.enabled = false
-	$Camera2D.enabled = true
 	ActionManager.run_actions(end_actions.data, self, null)
+
+
+func pause_vhs():
+	$Screen.material.set_shader_parameter("noise_opacity", 0.4)
+	
+
+func rewind_vhs():
+	$Screen.material.set_shader_parameter("roll", true)
